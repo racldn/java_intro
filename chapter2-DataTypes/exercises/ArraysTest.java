a96b93075c72063e56945af2955e9c97b4828c00
Robbie Campbell <roberto251983@gmail.com>
2018-10-22 20:38:43 +0100

    javabuzz complete

---
+++ chapter2-DataTypes/exercises/ArraysTest.java
@@ -0,0 +1,31 @@
+import java.util.Arrays
+// can you remember how to set up your main function?
+public class ArraysTest {
+    public static void main(String[] args) {
+// declare an array of Strings and print it
+        String[] arrayStr = new String[]{"Java", "Node", "Python", "Ruby"};
+        Arrays.stream(arrayStr).forEach(System.out::println);
+
+
+
+
+
+
+
+//
+
+
+        //
+
+
+        //declare an array of integers and print it
+
+
+        //declare an ArrayList of integers, add numbers to it, and then print it
+
+
+        //
+
+
+    }
+}
--- chapter3-JavaBuzz/exercises/ControlFlow.java
+++ chapter3-JavaBuzz/exercises/ControlFlow.java
@@ -1,10 +1,25 @@
 // what imports will you need?
-
+import java.util.Scanner;
 // can you remember how to set up your main function?
+public class ControlFlow {
+    public static void main ( String[] args ) {
 
 // find a way to read in user input
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
 
 // write a method that will check if a number is odd or even
 // (assume user only ever enters integers)
 
+
 // print the answer to the console
+        if((2 % number) == 0) {
+
+            System.out.println("Even");
+
+        }else {
+            System.out.println("Odd");
+        }
+
+    }
+}
